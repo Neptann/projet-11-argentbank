2849347ad2e054ee38588fedd34d538494de8938
clement brossier <clement.audiovisuel@gmail.com>
2023-10-02 12:08:46 +0200

    refactor(src): adding home, login, user pages and react router

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/home/home";
+import Login from "./assets/pages/login/login";
+import User from "./assets/pages/user/user";
 
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="user" element={<User />} />
+        <Route path="*" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ front/src/assets/pages/user/user.js
@@ -0,0 +1,7 @@
+import "../user/user.css";
+
+function User() {
+  return;
+}
+
+export default User;
