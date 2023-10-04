b84d4d83e963fc9e671023efff85e85344214ef5
clement brossier <clement.audiovisuel@gmail.com>
2023-10-04 10:35:07 +0200

    feat(src): adding routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./assets/pages/home/home";
 import Login from "./assets/pages/login/login";
 import User from "./assets/pages/user/user";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="user" element={<User />} />
-        <Route path="*" />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/user/:id" element={<User />} />
+      <Route path="*" />
+    </Routes>
   );
 }
 
