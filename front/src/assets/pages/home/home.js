ea50ceeb6d897b704fdd13eab2bf525b6bb37a10
clement brossier <clement.audiovisuel@gmail.com>
2023-10-04 10:29:26 +0200

    feat: adding home and login pages

--- front/src/assets/pages/home/home.js
+++ front/src/assets/pages/home/home.js
@@ -1,7 +1,18 @@
 import "../home/home.css";
+import Header from "../../components/header/header";
+import Banner from "../../components/banner/banner";
+import Feature from "../../components/feature/feature.js";
+import Footer from "../../components/footer/footer";
 
 function Home() {
-  return <h1>HOME</h1>;
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Feature />
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
--- front/src/assets/pages/login/login.js
+++ front/src/assets/pages/login/login.js
@@ -1,7 +1,22 @@
 import "../login/login.css";
+import Header from "../../components/header/header";
+import Form from "../../components/form/form";
+import Footer from "../../components/footer/footer";
 
 function Login() {
-  return;
+  return (
+    <div className="login">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Form />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default Login;
